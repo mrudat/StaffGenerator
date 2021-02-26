a8615024df10a4d8ffeeee1679ba3737f99fca7d
Martin Rudat <mrudat@toraboka.com>
2021-02-26 19:51:28 +1100

    Initial version

---
+++ StaffGenerator/Settings.cs
@@ -0,0 +1,19 @@
+﻿using Mutagen.Bethesda;
+using Mutagen.Bethesda.Skyrim;
+using System.Collections.Generic;
+
+namespace StaffGenerator
+{
+    public record Settings
+    {
+        public string StaffNamePrefix = "";
+
+        public string StaffNameSuffix = " Staff";
+
+        public HashSet<FormLink<IBookGetter>> SpellBooksToIgnore = new();
+
+        public HashSet<FormLink<ISpellGetter>> SpellsToIgnore = new();
+
+        public HashSet<FormLink<IWeaponGetter>> StavesToIgnore = new();
+    }
+}
